c85346d639fb2791637c30cf797c0099236f18c7
Ncebakazi Somdaka <ncesh.somdaka1@gmail.com>
2024-04-13 04:16:27 +0200

    On branch main  Changes to be committed: 	new file:   config/corsOptions.js 	new file:   controllers/authController.js 	new file:   controllers/employeesController.js 	new file:   controllers/refresTokenController.js 	new file:   controllers/registerController.js 	modified:   logs/reqLog.txt 	new file:   middleware/verifyJWT.js 	new file:   model/users.json 	modified:   package.json 	modified:   public/css/style.css 	modified:   routes/api/employees.js 	new file:   routes/auth.js 	new file:   routes/refresh.js 	new file:   routes/register.js 	modified:   server.js 	modified:   views/index.html

---
+++ config/corsOptions.js
@@ -0,0 +1,17 @@
+const allowedOrigins = [
+    'https://127.0.0.1.5500',
+    'http:192.168.1.65:3000',
+    'http://localhost:3500'
+];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(allowedOrigins.indexOf(origin) !== -1 || origin){
+            callback(null, true)
+        }else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+module.exports = corsOptions;
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
- const express = require('express')
+const express = require('express')
 const app = express();
 const path = require('path');
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions');
 const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT')
+const cookieParser = require('cookie-parser')
 const { logger } = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
 
 // custom middlerware
 app.use(logger)
+
+//Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
 //form data
 app.use(express.urlencoded({extended: false }));
@@ -14,14 +21,19 @@ app.use(express.urlencoded({extended: false }));
 //built middleware json
 app.use(express.json());
 
+//middleware for cookies
+app.use(cookieParser());
+
 //serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname });
-    
-})
+//routes
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
 
+app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
